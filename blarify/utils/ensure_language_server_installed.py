bd987dbff023b7f8238d816ac7d41f8b53b5cdb5
v4rgas <66626747+v4rgas@users.noreply.github.com>
2025-02-28 16:43:31 -0300

    refactor: remove initialize_all_language_servers.py and implement ensure_language_server_installed.py for language server management

---
+++ blarify/utils/ensure_language_server_installed.py
@@ -0,0 +1,36 @@
+import logging
+import os
+from pathlib import Path
+
+from blarify.vendor.multilspy import SyncLanguageServer
+
+from blarify.vendor.multilspy.multilspy_config import MultilspyConfig
+from blarify.vendor.multilspy.multilspy_logger import MultilspyLogger
+
+
+def ensure_language_server_installed(language: str):
+    """
+    Ensure that the language server for the given language is installed.
+    """
+
+    my_logger = logging.getLogger(__name__)
+
+    config = MultilspyConfig.from_dict({"code_language": language})
+    logger = MultilspyLogger()
+    current_dir_path = os.path.join(str(Path(__file__).resolve().parent))
+
+    print(f"Starting language server for {language}, current_dir_path: {current_dir_path}")
+
+    try:
+        print(f"Starting language server for {language}")
+        lsp = SyncLanguageServer.create(
+            config=config, logger=logger, repository_root_path=current_dir_path, timeout=15
+        )
+        with lsp.start_server():
+            my_logger.info(f"Started language server for {language}")
+    except Exception as e:
+        my_logger.error(f"Failed to start language server for {language}: {e}")
+
+
+if __name__ == "__main__":
+    ensure_language_server_installed("typescript")
