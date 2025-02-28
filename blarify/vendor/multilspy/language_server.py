2db523b7b8b211fc79d89c2888e78bf23edbf5f7
v4rgas <66626747+v4rgas@users.noreply.github.com>
2025-02-28 14:37:35 -0300

    fix: update type hinting for RelationshipType and refactor imports in language definitions and tree sitter helper feat: add initialization script for all language servers with logging fix: improve logging for empty responses in language server

--- blarify/vendor/multilspy/language_server.py
+++ blarify/vendor/multilspy/language_server.py
@@ -422,9 +422,9 @@ class LanguageServer:
                 }
             )
 
-        if not response:
+        if response is None:
             self.logger.log(
-                "Empty response received from Language Server for definitions, defaulting to empty list",
+                "'None' response received from Language Server for definition, defaulting to empty list",
                 logging.DEBUG,
             )
             response = []
@@ -522,9 +522,9 @@ class LanguageServer:
 
         ret: List[multilspy_types.Location] = []
 
-        if not response:
+        if response is None:
             self.logger.log(
-                "Empty response received from Language Server for references, defaulting to empty list",
+                "'None' response received from Language Server for references, defaulting to empty list",
                 logging.DEBUG,
             )
             response = []
