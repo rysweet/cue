a1e6278249c35cd20db780df9848ac0021f8a483
v4rgas <66626747+v4rgas@users.noreply.github.com>
2025-03-26 16:09:14 -0300

    feat: add parameter count calculation to CodeComplexityCalculator and integrate with FunctionNode

--- blarify/graph/node/function_node.py
+++ blarify/graph/node/function_node.py
@@ -1,4 +1,5 @@
 from blarify.graph.node import NodeLabels
+from blarify.stats.complexity import CodeComplexityCalculator
 from .types.definition_node import DefinitionNode
 
 
@@ -15,4 +16,5 @@ class FunctionNode(DefinitionNode):
         obj["attributes"]["start_line"] = self.node_range.range.start.line
         obj["attributes"]["end_line"] = self.node_range.range.end.line
         obj["attributes"]["text"] = self.code_text
+        obj["attributes"]["stats_paramater_count"] = CodeComplexityCalculator.calculate_parameter_count(self._tree_sitter_node)
         return obj
